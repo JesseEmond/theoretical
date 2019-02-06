295455e37b2a4bec6678d7b141720275cc3ee066
JesseEmond <emond.jesse@gmail.com>
2019-02-05 23:31:53 -0500

    Add tests for move last elements to end

--- merge/merge_test.py
+++ merge/merge_test.py
@@ -1,7 +1,7 @@
 import unittest
 from array_utils import is_sorted
 from merge import (_point_to_kth_biggest, _move_k_biggest_elements_to_end,
-                   SubarrayPointers)
+                   _move_last_elements_to_end, SubarrayPointers)
 
 
 def pointers_for(xs, ys, buffer_=None):
@@ -18,147 +18,202 @@ class PointToKthBiggestTests(unittest.TestCase):
         xs = [5, 7, 9, 10]
         ys = [1, 3, 4, 6, 8]
         A = xs + ys
-        self.assertEqual((1, 8),
-                         _point_to_kth_biggest(A, pointers_for(xs, ys), k=4))
+        self.assertEqual(_point_to_kth_biggest(A, pointers_for(xs, ys), k=4),
+                         (1, 8))
     def test_kth_biggest_in_ys(self):
         xs = [5, 7, 9, 10]
         ys = [1, 3, 4, 6, 8]
         A = xs + ys
-        self.assertEqual((1, 7),
-                         _point_to_kth_biggest(A, pointers_for(xs, ys), k=5))
+        self.assertEqual(_point_to_kth_biggest(A, pointers_for(xs, ys), k=5),
+                         (1, 7))
+                         
     def test_kth_xs_exhausted(self):
         xs = [7, 9, 10]
         ys = [1, 3, 4, 6, 8]
         A = xs + ys
-        self.assertEqual((0, 6),
-                         _point_to_kth_biggest(A, pointers_for(xs, ys), k=5))
+        self.assertEqual(_point_to_kth_biggest(A, pointers_for(xs, ys), k=5),
+                         (0, 6))
     def test_kth_ys_exhausted(self):
         xs = [1, 7, 10]
         ys = [8]
         A = xs + ys
-        self.assertEqual((1, 3),
-                         _point_to_kth_biggest(A, pointers_for(xs, ys), k=3))
+        self.assertEqual(_point_to_kth_biggest(A, pointers_for(xs, ys), k=3),
+                         (1, 3))
     def test_kth_no_large_in_xs(self):
         xs = [1, 2, 3]
         ys = [4, 6, 8]
         A = xs + ys
-        self.assertEqual((3, 4),
-                         _point_to_kth_biggest(A, pointers_for(xs, ys), k=2))
+        self.assertEqual(_point_to_kth_biggest(A, pointers_for(xs, ys), k=2),
+                         (3, 4))
     def test_kth_no_large_in_ys(self):
         xs = [4, 6, 8]
         ys = [1, 2, 3]
         A = xs + ys
-        self.assertEqual((1, 6),
-                         _point_to_kth_biggest(A, pointers_for(xs, ys), k=2))
+        self.assertEqual(_point_to_kth_biggest(A, pointers_for(xs, ys), k=2),
+                         (1, 6))
 
 
-# TODO: move most of these to MoveLastElementsToEndTests
+class MoveLastElementsToEndTests(unittest.TestCase):
+    def test_move_both_xs_ys(self):
+        xs = [5, 7, 9, 10]
+        ys = [1, 3, 4, 6, 8]
+        A = xs + ys
+        new_pointers = _move_last_elements_to_end(A, pointers_for(xs, ys),
+                                                  xs_to_move=2, ys_to_move=3)
+        self.assertEqual(A, [5, 7, 1, 3, 9, 10, 4, 6, 8])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=2,
+                                          ys_start=2, ys_length=2,
+                                          buffer_start=4, buffer_length=5))
+    def test_move_only_xs(self):
+        xs = [5, 7, 9, 10]
+        ys = [1, 3, 4, 6, 8]
+        A = xs + ys
+        new_pointers = _move_last_elements_to_end(A, pointers_for(xs, ys),
+                                                  xs_to_move=2, ys_to_move=0)
+        self.assertEqual(A, [5, 7, 1, 3, 4, 6, 8, 9, 10])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=2,
+                                          ys_start=2, ys_length=5,
+                                          buffer_start=7, buffer_length=2))
+    def test_move_only_ys(self):
+        xs = [5, 7, 9, 10]
+        ys = [1, 3, 4, 6, 8]
+        A = xs + ys
+        new_pointers = _move_last_elements_to_end(A, pointers_for(xs, ys),
+                                                  xs_to_move=0, ys_to_move=3)
+        self.assertEqual(A, [5, 7, 9, 10, 1, 3, 4, 6, 8])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=4,
+                                          ys_start=4, ys_length=2,
+                                          buffer_start=6, buffer_length=3))
+    def test_move_all(self):
+        xs = [5, 7, 9, 10]
+        ys = [1, 3, 4, 6, 8]
+        A = xs + ys
+        new_pointers = _move_last_elements_to_end(A, pointers_for(xs, ys),
+                                                  xs_to_move=4, ys_to_move=5)
+        self.assertEqual(A, [5, 7, 9, 10, 1, 3, 4, 6, 8])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=0,
+                                          ys_start=0, ys_length=0,
+                                          buffer_start=0, buffer_length=9))
+    def test_move_nothing(self):
+        xs = [5, 7, 9, 10]
+        ys = [1, 3, 4, 6, 8]
+        A = xs + ys
+        new_pointers = _move_last_elements_to_end(A, pointers_for(xs, ys),
+                                                  xs_to_move=0, ys_to_move=0)
+        self.assertEqual(A, [5, 7, 9, 10, 1, 3, 4, 6, 8])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=4,
+                                          ys_start=4, ys_length=5,
+                                          buffer_start=9, buffer_length=0))
+    def test_move_both_with_existing_buffer(self):
+        xs = [5, 7, 9, 10]
+        ys = [1, 3, 4, 6, 8]
+        buffer_ = [15, 16]
+        A = xs + ys + buffer_
+        new_pointers = _move_last_elements_to_end(A,
+                                                  pointers_for(xs, ys, buffer_),
+                                                  xs_to_move=2, ys_to_move=3)
+        self.assertEqual(A, [5, 7, 1, 3, 9, 10, 4, 6, 8, 15, 16])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=2,
+                                          ys_start=2, ys_length=2,
+                                          buffer_start=4, buffer_length=7))
+
+
 class MoveKBiggestElementsToEndTests(unittest.TestCase):
-    # TODO: move to explicit tests instead
-    def _check_guarantees(self, A, xs, ys, new_pointers, k):
-        biggest_k = list(sorted(A))[-k:][:k]  # :k to handle k=0
-        buf = A[new_pointers.buffer_start:
-                new_pointers.buffer_start+new_pointers.buffer_length]
-        self.assertCountEqual(buf, biggest_k)  # end contains k biggest elems
-        new_xs = A[new_pointers.xs_start:
-                   new_pointers.xs_start+new_pointers.xs_length]
-        new_ys = A[new_pointers.ys_start:
-                   new_pointers.ys_start+new_pointers.ys_length]
-        self.assertEqual(len(new_xs)+len(new_ys), len(A)-k)
-        self.assertEqual(len(buf), k)
-        self.assertTrue(is_sorted(new_xs, 0, len(new_xs)),
-                        "xs not sorted: %s" % new_xs)
-        self.assertTrue(is_sorted(new_ys, 0, len(new_ys)),
-                        "ys not sorted: %s" % new_ys)
     def test_normal(self):
         xs = [5, 7, 9, 10]
         ys = [1, 3, 4, 6, 8]
         A = xs + ys
         new_pointers = _move_k_biggest_elements_to_end(A, pointers_for(xs, ys),
                                                        k=3)
-        self._check_guarantees(A, xs, ys, new_pointers, k=3)
-        self.assertEqual(SubarrayPointers(xs_start=0, xs_length=2,
+        self.assertEqual(A, [5, 7, 1, 3, 4, 6, 9, 10, 8])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=2,
                                           ys_start=2, ys_length=4,
-                                          buffer_start=6, buffer_length=3),
-                         new_pointers)
+                                          buffer_start=6, buffer_length=3))
     def test_biggest_all_in_xs(self):
         xs = [5, 8, 9, 10]
         ys = [1, 3, 4, 6, 7]
         A = xs + ys
         new_pointers = _move_k_biggest_elements_to_end(A, pointers_for(xs, ys),
                                                        k=3)
-        self._check_guarantees(A, xs, ys, new_pointers, k=3)
-        self.assertEqual(SubarrayPointers(xs_start=0, xs_length=1,
+        self.assertEqual(A, [5, 1, 3, 4, 6, 7, 8, 9, 10])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=1,
                                           ys_start=1, ys_length=5,
-                                          buffer_start=6, buffer_length=3),
-                         new_pointers)
+                                          buffer_start=6, buffer_length=3))
     def test_biggest_all_in_ys(self):
         xs = [4, 5, 6, 7]
         ys = [1, 3, 8, 9, 10]
         A = xs + ys
         new_pointers = _move_k_biggest_elements_to_end(A, pointers_for(xs, ys),
                                                        k=3)
-        self._check_guarantees(A, xs, ys, new_pointers, k=3)
-        self.assertEqual(SubarrayPointers(xs_start=0, xs_length=4,
+        self.assertEqual(A, [4, 5, 6, 7, 1, 3, 8, 9, 10])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=4,
                                           ys_start=4, ys_length=2,
-                                          buffer_start=6, buffer_length=3),
-                         new_pointers)
+                                          buffer_start=6, buffer_length=3))
     def test_xs_less_than_k_elements(self):
         xs = [4, 5, 6, 7]
         ys = [1, 3, 8, 9, 10]
         A = xs + ys
         new_pointers = _move_k_biggest_elements_to_end(A, pointers_for(xs, ys),
                                                        k=5)
-        self._check_guarantees(A, xs, ys, new_pointers, k=5)
-        self.assertEqual(SubarrayPointers(xs_start=0, xs_length=2,
+        self.assertEqual(A, [4, 5, 1, 3, 6, 7, 8, 9, 10])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=2,
                                           ys_start=2, ys_length=2,
-                                          buffer_start=4, buffer_length=5),
-                         new_pointers)
+                                          buffer_start=4, buffer_length=5))
     def test_ys_less_than_k_elements(self):
         xs = [4, 5, 6, 7, 11, 12, 13]
         ys = [1, 3, 8, 9, 10]
         A = xs + ys
         new_pointers = _move_k_biggest_elements_to_end(A, pointers_for(xs, ys),
                                                        k=6)
-        self._check_guarantees(A, xs, ys, new_pointers, k=6)
-        self.assertEqual(SubarrayPointers(xs_start=0, xs_length=4,
+        self.assertEqual(A, [4, 5, 6, 7, 1, 3, 11, 12, 13, 8, 9, 10])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=4,
                                           ys_start=4, ys_length=2,
-                                          buffer_start=6, buffer_length=6),
-                         new_pointers)
+                                          buffer_start=6, buffer_length=6))
     def test_xs_exhausted(self):
         xs = [4, 5, 6, 7]
         ys = [1, 2, 3]
         A = xs + ys
         new_pointers = _move_k_biggest_elements_to_end(A, pointers_for(xs, ys),
                                                        k=5)
-        self._check_guarantees(A, xs, ys, new_pointers, k=5)
-        self.assertEqual(SubarrayPointers(xs_start=0, xs_length=0,
+        self.assertEqual(A, [1, 2, 4, 5, 6, 7, 3])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=0,
                                           ys_start=0, ys_length=2,
-                                          buffer_start=2, buffer_length=5),
-                         new_pointers)
+                                          buffer_start=2, buffer_length=5))
     def test_ys_exhausted(self):
         xs = [1, 2, 3]
         ys = [4, 5, 6, 7]
         A = xs + ys
         new_pointers = _move_k_biggest_elements_to_end(A, pointers_for(xs, ys),
                                                        k=5)
-        self._check_guarantees(A, xs, ys, new_pointers, k=5)
-        self.assertEqual(SubarrayPointers(xs_start=0, xs_length=2,
+        self.assertEqual(A, [1, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=2,
                                           ys_start=2, ys_length=0,
-                                          buffer_start=2, buffer_length=5),
-                         new_pointers)
+                                          buffer_start=2, buffer_length=5))
     def test_k_0(self):
         xs = [4, 5, 6, 7]
         ys = [1, 3, 8, 9, 10]
         A = xs + ys
         new_pointers = _move_k_biggest_elements_to_end(A, pointers_for(xs, ys),
                                                        k=0)
-        self._check_guarantees(A, xs, ys, new_pointers, k=0)
-        self.assertEqual(SubarrayPointers(xs_start=0, xs_length=4,
+        self.assertEqual(A, [4, 5, 6, 7, 1, 3, 8, 9, 10])
+        self.assertEqual(new_pointers,
+                         SubarrayPointers(xs_start=0, xs_length=4,
                                           ys_start=4, ys_length=5,
                                           buffer_start=9,
-                                          buffer_length=0),
-                         new_pointers)
+                                          buffer_length=0))
 
 
 if __name__ == "__main__":
